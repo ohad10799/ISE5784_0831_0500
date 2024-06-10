e59fc31a526521af3f330775dc90d6939abbabe8
ohad10799 <oad10799@gmail.com>
2024-06-10 17:28:52 +0300

    stage 4 finished part 2

--- src/renderer/Camera.java
+++ src/renderer/Camera.java
@@ -2,6 +2,10 @@ package renderer;
 
 import primitives.*;
 
+import java.util.MissingResourceException;
+
+import static primitives.Util.isZero;
+
 public class Camera implements Cloneable {
 
 
@@ -55,6 +59,65 @@ public class Camera implements Cloneable {
 
 
     public static class Builder{
+
+        private final Camera camera = new Camera();
+
+        public Builder setLocation(Point location) {
+            camera.location = location;
+            return this;
+        }
+
+        public Builder setDirection(Vector vTo, Vector vUp) {
+           if (vTo.dotProduct(vUp) != 0)
+               throw new IllegalArgumentException("vTo and vUp are not orthogonal");
+            camera.vTo = vTo.normalize();
+            camera.vUp = vUp.normalize();
+            return this;
+        }
+
+        public Builder setVpSize(double width, double height) {
+            camera.width = width;
+            camera.height = height;
+            return this;
+        }
+
+        public Builder setVpDistance(double distance) {
+            camera.distance = distance;
+            return this;
+        }
+
+
+        //************************ need check if 0!!!! ************************
+        public Camera build() {
+            if (camera.location == null)
+                throw new MissingResourceException("Missing parameter", "Camera", "location");
+            if (camera.vTo == null)
+                throw new MissingResourceException("Missing parameter", "Camera", "vTo");
+            if (camera.vUp == null)
+                throw new MissingResourceException("Missing parameter", "Camera", "vUp");
+            if (isZero(camera.width))
+                throw new MissingResourceException("Missing parameter", "Camera", "width");
+            if (isZero(camera.height))
+                throw new MissingResourceException("Missing parameter", "Camera", "height");
+            if (isZero(camera.distance))
+                throw new MissingResourceException("Missing parameter", "Camera", "distance");
+            if (camera.width<0){
+                throw new IllegalArgumentException("width can't be negative");
+            }
+            if (camera.height<0){
+                throw new IllegalArgumentException("height can't be negative");
+            }
+            if (camera.distance<0){
+                throw new IllegalArgumentException("distance can't be negative");
+            }
+            // **************** check if needed!! *****************
+            if (camera.vTo.dotProduct(camera.vUp) != 0)
+                throw new IllegalArgumentException("vTo and vUp are not orthogonal");
+
+            camera.vRight = camera.vTo.crossProduct(camera.vUp).normalize();
+            return (Camera) camera.clone();
+        }
+
         public Builder setRayTracer(SimpleRayTracer test) {
             return this;
         }
--- unittests/renderer/CameraTest.java
+++ unittests/renderer/CameraTest.java
@@ -1,4 +1,7 @@
 package renderer;
+import org.junit.jupiter.api.Test;
+import primitives.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Testing Camera Class
@@ -7,8 +10,8 @@ package renderer;
 class CameraTest {
    /** Camera builder for the tests */
    private final Camera.Builder cameraBuilder = Camera.getBuilder()
-      .setRayTracer(new SimpleRayTracer(new Scene("Test")))
-      .setImageWriter(new ImageWriter("Test", 1, 1))
+    //  .setRayTracer(new SimpleRayTracer(new Scene("Test")))
+    //  .setImageWriter(new ImageWriter("Test", 1, 1))
       .setLocation(Point.ZERO)
       .setDirection(new Vector(0, 0, -1), new Vector(0, -1, 0))
       .setVpDistance(10);
